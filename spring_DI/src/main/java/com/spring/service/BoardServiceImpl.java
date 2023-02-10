7585d5a5ec67c7c08bb4a6ff7d922ce6263f018b
hmk904 <hmk904@naver.com>
2023-02-10 09:35:55 +0900

    spring_DI : modify boardservice modify application-context.xml

--- spring_DI/src/main/java/com/spring/service/BoardServiceImpl.java
+++ spring_DI/src/main/java/com/spring/service/BoardServiceImpl.java
@@ -10,6 +10,7 @@ import com.jsp.command.SearchCriteria;
 import com.jsp.dto.BoardVO;
 import com.jsp.service.BoardService;
 import com.spring.dao.BoardDAO;
+import com.spring.dao.ReplyDAO;
 
 public class BoardServiceImpl implements BoardService{
 	
@@ -17,19 +18,33 @@ public class BoardServiceImpl implements BoardService{
 	public void setBoardDAO(BoardDAO boardDAO) {
 		this.boardDAO = boardDAO;
 	}
+	private ReplyDAO replyDAO;
+	public void setReplyDAO(ReplyDAO replyDAO) {
+		this.replyDAO = replyDAO;
+	}
 	
 	@Override
 	public Map<String, Object> getBoardList(SearchCriteria cri) throws SQLException {
 		Map<String, Object> dataMap = new HashMap<String, Object>();
-		
+
+		// 현재 page 번호에 맞는 리스트를 perPageNum 개수 만큼 가져오기.
 		List<BoardVO> boardList = boardDAO.selectSearchBoardList(cri);
-		dataMap.put("boardList", boardList);
-		
+		// reply count 입력
+		for (BoardVO board : boardList) {
+			int replycnt = replyDAO.countReply(board.getBno());
+			board.setReplycnt(replycnt);
+		}
+		// 전체 board 개수
+		int totalCount = boardDAO.selectSearchBoardListCount(cri);
+
+		// PageMaker 생성.
 		PageMaker pageMaker = new PageMaker();
 		pageMaker.setCri(cri);
-		pageMaker.setTotalCount(boardDAO.selectSearchBoardListCount(cri));
+		pageMaker.setTotalCount(totalCount);
+
+		dataMap.put("boardList", boardList);
 		dataMap.put("pageMaker", pageMaker);
-		
+
 		return dataMap;
 	}
 
